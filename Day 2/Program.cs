8d283a13bd51db22083411fc7a67a3a58dd56c6b
Hamza Basraoui <hamzabasraoui@outlook.com>
2024-12-05 00:44:51 +0100

    Day 1 -> Day 3

---
+++ Day 2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Problem statement: https://adventofcode.com/2024/day/2
+const string inputFilePath = "input.txt";
+
+var safeReports = File.ReadLines(inputFilePath)
+    .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList())
+    .Count(IsSafeReportWithProblemDampener);
+
+Console.WriteLine(safeReports);
+return;
+
+// To solve part 2
+bool IsSafeReportWithProblemDampener(IReadOnlyList<int> levels)
+{
+    if (IsSafeReport(levels))
+    {
+        return true;
+    }
+
+    for (var i = 0; i < levels.Count; i++)
+    {
+        var copy = levels.ToList();
+        copy.RemoveAt(i);
+        if (IsSafeReport(copy))
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+// To solve part 1
+bool IsSafeReport(IReadOnlyList<int> levels)
+{
+    var possibleValues = new HashSet<int> { 1, 2, 3 };
+    var trend = levels[0] < levels[^1] ? 1 : -1;
+    var isSafe = true;
+    for (var i = 1; i < levels.Count; i++)
+    {
+        if (!possibleValues.Contains((levels[i] - levels[i - 1]) * trend))
+        {
+            isSafe = false;
+            break;
+        }
+    }
+    return isSafe;
+}
---
+++ Day 3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Problem statement: https://adventofcode.com/2024/day/3
+
+using System.Text.RegularExpressions;
+
+const string inputFilePath = "input.txt";
+
+var corruptedMemory = File.ReadAllText(inputFilePath);
+Console.WriteLine($"Sum of the results: {EValuateMulInstructionsWithConditionals(corruptedMemory)}");
+
+return;
+
+// To solve part 2
+int EValuateMulInstructionsWithConditionals(string input)
+{
+    var tokens = Regex.Split(input, @"(do\(\)|don't\(\))");
+    // At the beginning of the program, mul instructions are enabled.
+    var sum = EvaluateMulInstructions(tokens[0]);
+    
+    for (var i = 1; i < tokens.Length; i++)
+    {
+        if (tokens[i] == "don't()")
+        {
+            i++;
+            continue;
+        }
+        sum += EvaluateMulInstructions(tokens[i]);
+    }
+    
+    return sum;
+}
+
+// To solve part 1
+int EvaluateMulInstructions(string input)
+{
+    var regexp = new Regex(@"mul\((\d+),(\d+)\)");
+    var matches = regexp.Matches(input);
+    var sum = 0;
+    
+    foreach (var match in matches)
+    {
+        var groups = regexp.Match(match.ToString());
+        sum += int.Parse(groups.Groups[1].Value) * int.Parse(groups.Groups[2].Value); 
+    }
+    
+    return sum;
+}
